f4273a431b9815ba2fac0248d79f3799da03ecf7
alnig441 <alnig441@gmail.com>
2022-11-05 15:57:12 -0400

    new docs

--- docs/essentials/immutable/chunks/1-7b0ce0f8.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-4aaec6a5.js";export{t as component};
---
+++ docs/essentials/immutable/chunks/1-a6f9d236.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-d38985bb.js";export{t as component};
