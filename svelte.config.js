82915cf0f3988e31cf99c6d584f2a2f8bd93bfa1
alnig441 <alnig441@gmail.com>
2022-11-05 15:50:55 -0400

    mode checking in svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 // import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
 
-const dev = process.argv.includes('dev');
+const dev = (process.argv.includes('dev')||process.argv.includes('preview'));
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
