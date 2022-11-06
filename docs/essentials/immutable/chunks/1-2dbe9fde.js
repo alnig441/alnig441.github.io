6e43d48129ddfabe4e07c586e5d882aa34a48b0f
alnig441 <alnig441@gmail.com>
2022-11-06 17:07:12 -0500

    new docs - menu config update

---
+++ docs/essentials/immutable/chunks/1-2dbe9fde.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-9ea1e221.js";export{t as component};
--- docs/essentials/immutable/chunks/2-680fb178.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-cd301c0a.js";export{t as component};
