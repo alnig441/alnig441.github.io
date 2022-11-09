619d56e598541aafa207f718ec8bdb060df80119
alnig441 <alnig441@gmail.com>
2022-11-09 10:26:24 -0500

    new docs - codesoup component integration update

---
+++ docs/essentials/immutable/chunks/1-cd816dc3.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-226ca293.js";export{t as component};
--- docs/essentials/immutable/chunks/2-7bcd2c19.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-a4f6dca4.js";export{t as component};
