4f74b1197415dac595d56760cd2beb1429379400
alnig441 <alnig441@gmail.com>
2022-11-11 14:38:09 -0500

    new docs - codesoup componenst update

---
+++ docs/essentials/immutable/chunks/1-ed372791.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-4e31e9d5.js";export{t as component};
--- docs/essentials/immutable/chunks/2-4889fcee.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-fee5eaa0.js";export{t as component};
